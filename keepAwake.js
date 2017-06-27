1bb6cf535f377dc51647eafb2a5297a339caa8ec
Yan <gyan@informatica.com>
2017-06-27 16:07:16 -0400

    add keepawake

--- keepAwake.js
+++ keepAwake.js
@@ -1,4 +1,8 @@
 var http = require("http");
+var moment = require('moment');
+
 setInterval(function() {
-    http.get("https://dodotang-server.herokuapp.com/");
+	const time = moment().format('LL  @  HH:mm:ss')
+	console.log(`pinged on ${time}`);
+    //http.get("https://dodotang-server.herokuapp.com/");
 }, 300000); // every 5 minutes (300000)
